f3e4d1ea59b25ae95d8646d4cb0cba4fa49d8584
ppp991 <102567042+ppp991@users.noreply.github.com>
2025-09-24 10:42:33 -0700

    Add files via upload

---
+++ Physical_Constants_5.py
@@ -0,0 +1,95 @@
+
+# Gravitational constant
+G = 6.7e-8 # erg*cm/g^2
+
+# Speed of light
+c = 3e10 # cm/s
+
+# Planck's constant
+h = 6.6e-27 # erg*s
+h_bar = 1.05e-27
+
+# Boltzmann's constant
+k_erg = 1.4e-16 # erg/K
+k_eV = 8.6e-5 # eV/K
+
+# Stefan-Boltzmann constant
+sigma = 5.7e-5 # erg/cm^2*s*K^4
+
+# Radiation constant 
+a = 4*sigma/c # erg/cm^3*K^-4
+
+# Proton mass
+m_proton = 1.7e-24 # g
+
+# Electron mass
+m_electron = 9.1e-28 # g
+
+# Electron charge
+e = 4.8e-10 # esu
+
+# Electron volt conversion
+eV = 1.6e-12 # erg
+
+# Thomson cross section
+sigma_T = 6.7e-25 # cm^2
+
+# Wien's Law
+peak_lambda = 2900 # Angstroms
+peak_freq = 2.4/h # eV
+
+# Angstrom conversion
+A = 1e-8 # cm
+
+# Solar mass
+m_sol = 2e33 # g
+
+# Solar luminosity
+solar_lum = 3.8e33 # erg/s
+
+# Solar radius
+r_sol = 7e10 # cm
+
+# Solar distance
+d_sol = 1.5e13 # cm (1 AU)
+
+# Jupiter mass
+m_jupiter = 1.9e30 # g
+
+# Jupiter radius
+rad_jupiter = 7.0e9 # cm
+
+# Jupiter-Sun distance
+d_jup_sol = 7.8e13 # cm (5.2 AU)
+
+# Earth mass
+m_earth = 6.0e27 # g
+
+# Earth radius
+rad_earth = 6.4e8 # cm
+
+# Moon mass
+m_moon = 7.4e25 # g
+
+# Moon radius
+rad_moon = 1.7e8 # cm
+
+# Moon distance
+d_moon = 3.8e10 # cm
+
+# Astronomical unit conversion
+au = 1.5e13 # cm
+
+# Parsec conversion
+pc = 3.1e18 # cm
+
+# Year conversion
+yr = 3.15e7 # s
+
+
+
+
+
+
+
+
